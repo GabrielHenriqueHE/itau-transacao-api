d398624e1d3746ff510004a5920dce0e751432b0
Gabriel Evaristo <gabriel.evaristo.2004@gmail.com>
2025-02-22 11:20:11 -0300

    feat: initial commit

---
+++ src/test/java/io/github/gabrielhenriquehe/transacao_api/TransacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielhenriquehe.transacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
